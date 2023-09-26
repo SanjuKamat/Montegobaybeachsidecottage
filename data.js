4eabec76616b9235777e8f404f93e15cf7727c2a
Sanju Kamat <sanjukamat123@gmail.com>
2023-09-26 21:59:13 +0530

    first commit

---
+++ data.js
@@ -0,0 +1,307 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-bedroom",
+      "name": "Bedroom",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": 0.5299995905793047,
+        "pitch": -0.03444414680096486,
+        "fov": 1.3616874290001844
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.4636418100468083,
+          "pitch": 0.03374283301987013,
+          "rotation": 0,
+          "target": "1-bathroom"
+        },
+        {
+          "yaw": -1.8970726817868524,
+          "pitch": 0.10101230722387555,
+          "rotation": 0,
+          "target": "2-sofa-cum-bed"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-bathroom",
+      "name": "Bathroom",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": -1.791425609626529,
+        "pitch": 0.09108420096476877,
+        "fov": 1.3616874290001844
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.10248912831069035,
+          "pitch": 0.08991376009694285,
+          "rotation": 0,
+          "target": "5-bedroom"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-sofa-cum-bed",
+      "name": "Sofa Cum Bed",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": -1.0866242454124269,
+        "pitch": 0.2902172331212647,
+        "fov": 1.3616874290001844
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.6549796868050475,
+          "pitch": 0.15879406012395236,
+          "rotation": 0,
+          "target": "0-bedroom"
+        },
+        {
+          "yaw": -1.9500902868783196,
+          "pitch": 0.08457295095907824,
+          "rotation": 0,
+          "target": "3-beach-side-view"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-beach-side-view",
+      "name": "Beach Side VIew",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": 0.6485115250857909,
+        "pitch": -0.21707042040905478,
+        "fov": 1.3616874290001844
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.0896767569866839,
+          "pitch": 0.24798586305008996,
+          "rotation": 0,
+          "target": "4-beach-side-view"
+        },
+        {
+          "yaw": -2.2444720781621754,
+          "pitch": 0.06172684711039622,
+          "rotation": 0,
+          "target": "6-bedroom"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-beach-side-view",
+      "name": "Beach Side View",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": -1.7132064795977406,
+        "pitch": -0.2954408008566034,
+        "fov": 1.3616874290001844
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.109732949480878,
+          "pitch": -0.08854221108135896,
+          "rotation": 0,
+          "target": "6-bedroom"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-bedroom",
+      "name": "Bedroom",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": -1.6415172059655632,
+        "pitch": -0.03220876164025199,
+        "fov": 1.3616874290001844
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.4654537716192095,
+          "pitch": 0.054874841038529354,
+          "rotation": 0,
+          "target": "1-bathroom"
+        },
+        {
+          "yaw": -1.9402369224384053,
+          "pitch": 0.06650501827153477,
+          "rotation": 0,
+          "target": "2-sofa-cum-bed"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-bedroom",
+      "name": "Bedroom",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": 1.1504925477431982,
+        "pitch": 0.08671589672376001,
+        "fov": 1.3616874290001844
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.5893948098643849,
+          "pitch": 0.11982632121019066,
+          "rotation": 0,
+          "target": "0-bedroom"
+        },
+        {
+          "yaw": -1.8900967911738515,
+          "pitch": 0.10590485589595744,
+          "rotation": 5.497787143782138,
+          "target": "3-beach-side-view"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Beach Side Cottage",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
